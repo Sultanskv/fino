5478b0a0f18c9f6c98e0f1671f6ce71d1603d65a
Sultanskv <151192802+Sultanskv@users.noreply.github.com>
2024-05-09 17:35:26 +0530

    Update settings.py

--- algosms/algosms/settings.py
+++ algosms/algosms/settings.py
@@ -19,6 +19,8 @@ from pathlib import Path
 import os
 from environ import Env
 
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
